a2abedfa3a33879a08a78c3118974a7086b41541
Ian Phillips <ianjhphillips@gmail.com>
2026-03-27 12:49:10 -0400

    Refactor DB secrets into .env

--- server/src/db/connection.mjs
+++ server/src/db/connection.mjs
@@ -1,14 +1,15 @@
 import { MongoClient } from "mongodb";
 
-const connectionString = "mongodb://root:example@mongo:27017/";
+const connectionString = `${process.env.DB_CONN}://${process.env.DB_UNAME}:${process.env.DB_PWD}@${process.env.DB_HOST}:${process.env.DB_PORT}/`;
 
 const client = new MongoClient(connectionString);
 
 let conn;
 try {
+  console.log("Connecting DB client...")
   conn = await client.connect();
 } catch(e) {
-  console.error(e);
+  console.error(`ERROR: while attempting to connect to db client\n============\n${e}\n============\n`);
 }
 
 let db = conn.db("finance");
